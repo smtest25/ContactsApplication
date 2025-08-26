b7fc6af87e0e43a9c67a6115de72807b3e7a590c
@ <@>
2025-08-26 13:23:52 +0200

    add repositories

---
+++ Repositories/ContactRepository.cs
@@ -0,0 +1,81 @@
+﻿using ContactsApplication.Data;
+using ContactsApplication.Models;
+using System.Linq.Dynamic.Core;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactsApplication.Repositories
+{
+    public class ContactRepository(ContactContext context) : IContactRepository
+    {
+        private readonly ContactContext _context = context;
+
+        public async Task<bool?> UpdateContactAsync(ContactModel contactModel)
+        {
+            try
+            {
+                _context.Update(contactModel);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (await _context.Contacts.FindAsync(contactModel.ID) == null)
+                {
+                    return false;
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        public async Task<bool?> CreateContactAsync(ContactModel contactModel)
+        {
+            _context.Add(contactModel);
+            var added = await _context.SaveChangesAsync();
+            return added > 0;
+        }
+
+        public async Task<ContactModel?> GetContactAsync(int id)
+        {
+            return await _context.Contacts.FindAsync(id);
+        }
+
+        public async Task<List<ContactModel>> GetContactsAsync(string sort, string filter)
+        {
+            var sortProp = sort?.Split(":").First() ?? "ID";
+            var sortDir = sort?.Split(":").Last();
+
+            var contacts = _context.Contacts.AsQueryable();
+
+            foreach (var filterExpr in (filter ?? string.Empty).Split("|"))
+            {
+                var filterProp = filterExpr.Split(":").First();
+                var filterVal = filterExpr.Split(":").Last();
+                if (typeof(ContactModel).GetProperty(filterProp) != null)
+                {
+                    contacts = contacts.Where($"{filterProp}.Contains(@0)", filterVal);
+                }
+            }
+
+            if (typeof(ContactModel).GetProperty(sortProp) != null)
+                contacts = contacts.OrderBy($"{sortProp} {(sortDir == "d" ? "desc" : "asc")}");
+
+            return await contacts.ToListAsync();
+        }
+
+        public async Task<bool?> DeleteContactAsync(int id)
+        {
+            var contactModel = await _context.Contacts.FindAsync(id);
+            if (contactModel != null)
+            {
+                _context.Contacts.Remove(contactModel);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Repositories/IContactRepository.cs
@@ -0,0 +1,14 @@
+﻿using ContactsApplication.Models;
+
+namespace ContactsApplication.Repositories
+{
+    public interface IContactRepository
+    {
+        public Task<List<ContactModel>> GetContactsAsync(string sort, string filter);
+        public Task<ContactModel?> GetContactAsync(int id);
+        public Task<bool?> CreateContactAsync(ContactModel contactModel);
+        public Task<bool?> UpdateContactAsync(ContactModel contactModel);
+        public Task<bool?> DeleteContactAsync(int id);
+
+    }
+}
